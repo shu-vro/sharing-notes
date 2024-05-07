35202fd4b5dbe75e6aef937057821b37acdefd2f
shirshen <official.shirshen@gmail.com>
2024-05-07 23:36:53 +0600

    Initial commit from Create Next App

---
+++ firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAVwFfepJhIiuxVLer12ovdQqhfLYgIpi8",
+    authDomain: "note-share-dev.firebaseapp.com",
+    projectId: "note-share-dev",
+    storageBucket: "note-share-dev.appspot.com",
+    messagingSenderId: "876442271630",
+    appId: "1:876442271630:web:b76b15ecae2d7e4b722edf",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);

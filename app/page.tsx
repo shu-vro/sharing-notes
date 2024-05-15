019e8d3fbf5e058a4c6cf599f125d560b6815df8
shirshen <official.shirshen@gmail.com>
2024-05-16 02:39:41 +0600

    refactor: remove unused components and imports

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,5 @@
 "use client";
 
-import File from "./components/File";
-import Header from "./components/Header";
-import AddButtons from "./components/AddButtons";
-import FolderSection from "./components/FolderSection";
-import { BreadcrumbComponent } from "./components/BreadCrumb";
-import Ready from "./components/Ready";
 import { useRouter } from "next/navigation";
 
 export default function Home() {
